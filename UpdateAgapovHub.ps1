e1206834bb51e7bf13a4a5f3388e8c8a33054ac1
Igor Poiskov <rifoganjic@gmail.com>
2023-10-22 02:47:23 +0300

    an another updater fix

--- UpdateAgapovHub.ps1
+++ UpdateAgapovHub.ps1
@@ -1,7 +1,5 @@
-﻿cd $PWD
-git stash --all
-gh repo sync
-git stash pop
+﻿git fetch --all
+git reset --hard origin/master
 Read-Host -Prompt "Press Enter to exit"
 $key = $Host.UI.RawUI.ReadKey
 if ($key.Character -eq '1') {
